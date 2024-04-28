9d8c50d58489cc66814ea9db6be2453be09d0368
Naimul2580 <21201142@uap-bd.edu>
2024-04-28 21:48:50 +0600

    Owner Model Added

--- rentcar/models.py
+++ rentcar/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+ # Fields for owner sign up
+class Owner(models.Model): 
+    username = models.CharField(max_length=150, unique=True)
+    contact_no = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  
+
+    def __str__(self):
+        return self.username
--- rentcar/views.py
+++ rentcar/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Owner
 
 # Create your views here.
 def home(request):
