0b99558df013f5b6aafd7264bdd1019832676e13
yeakiniqra <21201128@uap-bd.edu>
2024-04-28 23:54:59 +0600

    upload car model create

--- rentcar/models.py
+++ rentcar/models.py
@@ -10,3 +10,29 @@ class Owner(models.Model):
 
     def __str__(self):
         return self.username
+    
+class UploadCar(models.Model):
+    CAPACITY_CHOICES = (
+        ('4', '4'),
+        ('6', '6'),
+        ('8', '8'),
+    )
+
+    CAR_TYPE_CHOICES = (
+        ('sedan', 'Sedan'),
+        ('suv', 'SUV'),
+        ('hatchback', 'Hatchback'),
+    )
+
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    car_name = models.CharField(max_length=150)
+    car_no = models.CharField(max_length=20)
+    mileage = models.CharField(max_length=20)
+    capacity = models.CharField(max_length=20, choices=CAPACITY_CHOICES)
+    car_image = models.ImageField(upload_to='uploadcar_images')
+    PricePerDay = models.IntegerField()
+    car_type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES)
+    car_condition = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.car_name       
