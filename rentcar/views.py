916f99b27d6d7fa70ae371241d394b6a7731b0ea
Naimul2580 <21201142@uap-bd.edu>
2024-04-28 21:38:04 +0600

    Owner Sign UP

--- rentcar/views.py
+++ rentcar/views.py
@@ -4,6 +4,37 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 def ownersignup(request):
+    if request.method == 'POST':
+       
+        username = request.POST.get('username')
+        contact_no = request.POST.get('contact_no')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        
+        if username and contact_no and email and password:
+           
+            new_owner = Owner(
+                username=username,
+                contact_no=contact_no,
+                email=email,
+                password=password
+            )
+            
+            new_owner.save()
+            
+            
+            request.session['owner_username'] = username
+            request.session['owner_contact_no'] = contact_no
+            request.session['owner_email'] = email
+            request.session['owner_password'] = password
+            
+            
+            return redirect('ownerlogin')  
+        else:
+            messages.error(request, 'All fields are required.')
+            return redirect('ownersignup')  
     return render(request, 'ownersignup.html')
+    
 def ownerprofile(request):
     return render(request, 'ownerprofile.html')
