880b10e8117a1dbd9a96eb44c6b75539e819d1f9
yeakiniqra <21201128@uap-bd.edu>
2024-04-29 00:07:17 +0600

    media updated

--- wheelzy/urls.py
+++ wheelzy/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rentcar.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name='home'),
@@ -29,6 +32,10 @@ urlpatterns = [
     path('ownerlogin/', ownerlogin, name='ownerlogin'),
     path('ownerlogout/', ownerlogout, name='ownerlogout'),
     path('booking_car/', booking_car, name='booking_car'),
-
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
