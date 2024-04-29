acc1d2648fe33bc6aff22c7a81db5e330abe489e
yeakiniqra <21201128@uap-bd.edu>
2024-04-29 22:20:35 +0600

    html added

--- wheelzy/settings.py
+++ wheelzy/settings.py
@@ -132,3 +132,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
